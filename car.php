b762a7e9b509ebc3fb149d99b6f9cf48c47ec274
wanglei <909521757@qq.com>
2018-12-03 12:06:54 +0800

    第一次提交wang.php

---
+++ car.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @Author: Marte
+ * @Date:   2018-12-03 12:06:15
+ * @Last Modified by:   Marte
+ * @Last Modified time: 2018-12-03 12:06:33
+ */
+function wang()
+{
+    echo  1111;
+}
